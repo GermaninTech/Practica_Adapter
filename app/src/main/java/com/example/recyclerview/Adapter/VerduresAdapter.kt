1582db2bac846e4017770d09b13c6608813eb138
German Rodriguez <wallcat2007@gmail.com>
2025-02-14 08:48:02 +0100

    Afegim Primeres Modificacions

---
+++ app/src/main/java/com/example/recyclerview/Adapter/VerduresAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.recyclerview.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.Data.Verdures
+import com.example.recyclerview.databinding.ItemVerduresBinding
+
+class VerduresAdapter(val VerduresList: MutableList<Verdures>) : RecyclerView.Adapter<VerduresAdapter.VerduresVh>() {
+    class VerduresVh(val binding: ItemVerduresBinding) : RecyclerView.ViewHolder(
+        binding.root
+    ){
+        companion object{
+            fun crear(parent: ViewGroup):VerduresVh{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemVerduresBinding.inflate(layoutInflater,parent,false)
+                return VerduresVh(binding)
+            }
+        }
+        fun emplenar(verdures: Verdures){
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VerduresVh {
+        return VerduresVh.crear(parent)
+    }
+
+    override fun getItemCount(): Int = VerduresList.size
+
+    override fun onBindViewHolder(holder: VerduresVh, position: Int) {
+        holder.emplenar(VerduresList[position])
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/Data/Verdures.kt
@@ -0,0 +1,7 @@
+package com.example.recyclerview.Data
+
+data class Verdures(
+    val nom:String,
+    val color:String,
+    val tipus:String
+)
