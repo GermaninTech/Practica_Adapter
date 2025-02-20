ce6090159d2f0a25c5db36d676908ba6aec2326e
German Rodriguez <wallcat2007@gmail.com>
2025-02-20 14:00:20 +0100

    ultimas modificaciones

--- app/src/main/java/com/example/recyclerview/Data/Verdures.kt
+++
@@ -1,7 +0,0 @@
-package com.example.recyclerview.Data
-
-data class Verdures(
-    val nom:String,
-    val color:String,
-    val tipus:String
-)
--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,14 +1,43 @@
 package com.example.recyclerview
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.Adapter.VerduresAdapter
+import com.example.recyclerview.Data.Verdura
+import com.example.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        val verduraList:MutableList<Verdura> = mutableListOf(
+            Verdura("Zanahoria", "Naranja", "Raíz"),
+            Verdura("Lechuga", "Verde", "Hoja"),
+            Verdura("Tomate", "Rojo", "Fruto"),
+            Verdura("Pepino", "Verde", "Fruto"),
+            Verdura("Cebolla", "Blanco", "Bulbo"),
+            Verdura("Berenjena", "Morado", "Fruto"),
+            Verdura("Espinaca", "Verde", "Hoja"),
+            Verdura("Brócoli", "Verde", "Flor"),
+            Verdura("Coliflor", "Blanco", "Flor"),
+            Verdura("Pimiento", "Rojo", "Fruto")
+        )
+        val verduraAdapter = VerduresAdapter(verduraList)
+        binding.RVverdura.adapter = verduraAdapter
+        binding.RVverdura.layoutManager = LinearLayoutManager(this)
+
+    }
+
+}
+
